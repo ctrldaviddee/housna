16a040270740ac78778684399e6e37ee487bd3ff
ctrldaviddee <princedavealex.20@gmail.com>
2025-03-07 18:43:52 +0100

    footer okay

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@ cursor.classList.add('custom-cursor');
 document.body.appendChild(cursor);
 
 document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.clientX + 'px';
-    cursor.style.top = e.clientY + 'px';
+  cursor.style.left = e.clientX + 'px';
+  cursor.style.top = e.clientY + 'px';
 });
 
 
@@ -19,33 +19,27 @@ document.addEventListener('mousemove', (e) => {
 <template>
   <div class="min-h-screen bg-neutral-950">
     <Header />
-    <div style="border-top-left-radius:50px;border-top-right-radius:50px" class="relative flex flex-auto overflow-hidden bg-white top-2">
-      
-    <div class="relative isolate flex w-full flex-col ">
-    <RouterView v-slot="{ Component }">
-      <Transition
-        enter-active-class="transition duration-300 ease-out"
-        enter-from-class="transform opacity-0"
-        enter-to-class="transform opacity-100"
-        leave-active-class="transition duration-200 ease-in"
-        leave-from-class="transform opacity-100"
-        leave-to-class="transform opacity-0"
-      >
-        <component :is="Component" />
-      </Transition>
+    <div style="border-top-left-radius:50px;border-top-right-radius:50px"
+      class="relative flex flex-auto overflow-hidden bg-white top-2">
 
-    </RouterView>
-    
-<Footer/>
+      <div class="relative isolate flex w-full flex-col ">
+        <RouterView v-slot="{ Component }">
+          <Transition enter-active-class="transition duration-300 ease-out" enter-from-class="transform opacity-0"
+            enter-to-class="transform opacity-100" leave-active-class="transition duration-200 ease-in"
+            leave-from-class="transform opacity-100" leave-to-class="transform opacity-0">
+            <component :is="Component" />
+          </Transition>
+
+        </RouterView>
+
+        <Footer />
+      </div>
     </div>
-  </div>
-    
 
-    
+
+
   </div>
 </template>
 
 
-<style>
-
-</style>
+<style></style>
